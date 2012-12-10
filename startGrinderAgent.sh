18b65fcc8ef565e881a21a94deacb7e11eec25ba
Rob Cruz <aleksei.r.cruz@rrd.com>
2012-12-10 10:30:28 -0800

    modified startGrinderAgent.sh to include grinder log direction on command line

--- startGrinderAgent.sh
+++ startGrinderAgent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo java -cp lib/grinder.jar:lib/grinder-agent.jar:lib/jython.jar:lib/grinder-xmlbeans.jar:lib/jsr173_1.0_api.jar:lib/picocontainer-1.3.jar:lib/xbean.jar net.grinder.Grinder $1
-java -cp lib/grinder.jar:lib/grinder-agent.jar:lib/jython.jar:lib/grinder-xmlbeans.jar:lib/jsr173_1.0_api.jar:lib/picocontainer-1.3.jar:lib/xbean.jar net.grinder.Grinder $1
+java -cp lib/grinder.jar:lib/grinder-agent.jar:lib/jython.jar:lib/grinder-xmlbeans.jar:lib/jsr173_1.0_api.jar:lib/picocontainer-1.3.jar:lib/xbean.jar -Dgrinder.logDirectory="log/$(date +%y%m%d)" net.grinder.Grinder $1
