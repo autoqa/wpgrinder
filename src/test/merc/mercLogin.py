d4c10f0a9f106f6ff2b95b4695be7f8b39d6a061
Rob Cruz <aleksei.r.cruz@rrd.com>
2012-12-13 16:22:36 -0800

    initial commit

---
+++ src/test/merc/mercLogin.py
@@ -0,0 +1,16 @@
+__author__ = 'robertc'
+from net.grinder.script import Test
+from net.grinder.script.Grinder import grinder
+from net.grinder.plugin.http import HTTPPluginControl, HTTPRequest
+from HTTPClient import NVPair
+
+connectionDefaults = HTTPPluginControl.getConnectionDefaults()
+httpUtilities = HTTPPluginControl.getHTTPUtilities()
+
+def createRequest(test, url, headers=None):
+    """Create an instrumented HTTPRequest."""
+    request = HTTPRequest(url=url)
+    if headers: request.headers=headers
+    test.record(request, HTTPRequest.getHttpMethodFilter())
+    return request
+
